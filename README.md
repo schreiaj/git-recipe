c57cb382620670ecff61e6ac51bdba18fe8d1b92
Andrew Schreiber <aschreiber@mitre.org>
2013-08-29 14:16:52 -0400

    Added foodcritic for testing and set up for travis-ci

---
+++ README.md
@@ -0,0 +1,4 @@
+Git Cookbook
+============
+
+This cookbook installs git on your system.
---
+++ metadata.rb
@@ -0,0 +1,4 @@
+name "git"
+maintainer "Andrew Schreiber"
+maintainer_email "aschreiber@mitre.org"
+description "Installs git"
